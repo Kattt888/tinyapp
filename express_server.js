4f544fb976f60ac99991aaa33881cb5384e26bd6
Katrina <katrinabyrd88@gmail.com>
2024-11-04 20:01:31 -0500

    updated with URL shortening part 1

--- express_server.js
+++ express_server.js
@@ -4,10 +4,16 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,11 +32,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
   const templateVars = { id: id, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 
